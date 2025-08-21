798dd2c1e77518e84b40d22e858578b7761112c9
Abhijeet Srivastava <selvester69@gmail.com>
2025-08-21 23:18:30 +0530

    Revert "Feat: Add a comprehensive set of new UI components"

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Documentation from "./pages/Documentation";
 import NotFound from "./pages/NotFound";
-import ViewDocument from "./pages/ViewDocument";
 
 const queryClient = new QueryClient();
 
@@ -18,8 +16,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/viewDocument" element={<ViewDocument />} />
-          <Route path="/documentation" element={<Documentation />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
